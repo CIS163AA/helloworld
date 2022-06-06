1861007f283e63bfb92311c06b050b3a3b0ecb05
taniactl <99217823+taniactl@users.noreply.github.com>
2022-06-06 10:24:08 -0700

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Helloworld {
+
+  public static void main(String[] args) {
+
+
+String c="What is your name?";
+
+   c = JOptionPane.showInputDialog(
+   "What is your name?");
+   
+System.out.println(c); 
+   }
+}
